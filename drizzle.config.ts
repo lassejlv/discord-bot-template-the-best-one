eff46f3f96b86e6ae158c8b3509a74dce34c0a77
lasse <77295879+lassejlv@users.noreply.github.com>
2024-06-21 00:31:06 +0200

    changes lol

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,5 @@
 import { defineConfig } from "drizzle-kit";
 
-// export default defineConfig({
-//   schema: "./src/db/schemas/**/*",
-//   out: "./src/db/migrations",
-//   dialect: "sqlite",
-//   driver: "turso",
-//   dbCredentials: {
-//     url: process.env.TURSO_CONNECTION_URL!,
-//     authToken: process.env.TURSO_AUTH_TOKEN!,
-//   },
-// });
-
 export default defineConfig({
   schema: "./src/db/schemas/**/*",
   out: "./src/db/migrations",
